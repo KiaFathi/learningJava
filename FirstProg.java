5257787086e77c05df3137bf2c2050ba0508aca7
Kia Fathi <kiafathi@me.com>
2014-11-23 14:00:00 +0100

    (feat) Complete some basic Java exercises

---
+++ FirstProg.java
@@ -0,0 +1,8 @@
+public class FirstProg
+{
+  public static void main( String[] args)
+  {
+    System.out.println("I, Kia Fathi, am determined to larn how to code in Java.");
+    System.out.println("Today's date is: 11/19/14");
+  }
+}
---
+++ LetterToYourself.java
@@ -0,0 +1,18 @@
+public class LetterToYourself
+{
+  public static void main ( String [] args )
+  {
+    System.out.println("+------------------------------------------+");
+    System.out.println("|                                    ####  |");
+    System.out.println("|                                    ####  |");
+    System.out.println("|                                    ####  |");
+    System.out.println("|                                          |");
+    System.out.println("|                                          |");
+    System.out.println("|                                          |");
+    System.out.println("|                     Kia Fathi            |");
+    System.out.println("|                     Super Awesome Way    |");
+    System.out.println("|                     America              |");
+    System.out.println("|                                          |");
+    System.out.println("+------------------------------------------+");
+  }
+} 
